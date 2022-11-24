77d8b96742df603f046f810773a4293a7b82fac5
Musharraf8543 <musharrafhussain4397@gmail.com>
2022-11-24 12:39:56 +0530

    heade in bteween

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import spinner from '../../img/spinner.gif'
+import spinner from '../../assets/spinner.gif'
 const Spinner = () => {
   return (
     <img src={spinner} style={{width:'100px', margin:'auto',display:'block'}} alt='Loading'/>
